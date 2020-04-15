cc2285c0b6229a3c9a49964a36e958bfec31eda3
Hun Zi-Song <zhun0001@student.monash.edu>
2020-04-16 02:43:37 +0800

    Added widgets file to export all widgets at once so that only one file is imported every time

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,6 @@
+/// DO NOT ADD ANY WIDGET IN THIS FILE.
+/// IT IS SOLELY FOR THE PURPOSE OF EXPORTING WIDGETS SO THAT ONLY ONE FILE IS IMPORTED
+/// P.S. add the path of widgets created here
+
+export 'package:heard/widgets/input_field.dart';
+export 'package:heard/widgets/user_button.dart';
