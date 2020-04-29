feb23d33301d6fad99c00615e92885433851d588
Hun Zi-Song <zhun0001@student.monash.edu>
2020-04-30 07:38:22 +0800

    Made some UI tweaks for InputField widget

--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -4,17 +4,25 @@ import 'package:heard/constants.dart';
 class InputField extends StatelessWidget {
   final isPassword;
   final labelText;
+  final TextEditingController controller;
 
-  InputField({this.isPassword = false, this.labelText = ''});
+  InputField({this.isPassword = false, this.labelText = '', @required this.controller});
 
   Widget build(BuildContext context) {
     return Padding(
-      padding: Paddings.vertical_15,
+      padding: Paddings.vertical_18,
       child: TextField(
+         controller: controller,
           obscureText: isPassword,
           decoration: InputDecoration(
             contentPadding: Paddings.horizontal_5,
-            labelText: labelText
+            labelText: labelText,
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(width: 3.0, color: Colours.grey)
+            ),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(width: 3.0, color: Colours.blue)
+            ),
           )),
     );
   }
