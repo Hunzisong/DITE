ced5b1c2747830dcdd1dde54244f1a80bde9bb55
Hun Zi-Song <zhun0001@student.monash.edu>
2020-05-04 17:43:25 +0800

    Added two buttons for SLI and user each to sign up for startup page

--- lib/startup/startup_page.dart
+++ lib/startup/startup_page.dart
@@ -10,51 +10,65 @@ class StartupPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: Paddings.startupMain,
-          child: ListView(
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              SizedBox(
-                height: Dimensions.d_15,
-              ),
-              Container(
-                padding: Paddings.vertical_15,
-                height: Dimensions.d_280,
-                child: Hero(
-                  tag: 'appLogo',
-                  child: Image(
-                    image: AssetImage('images/diteLogo.png'),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: Paddings.startupMain,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: Dimensions.d_15,
                   ),
-                ),
+                  Container(
+                    padding: Paddings.vertical_15,
+                    height: Dimensions.d_280,
+                    child: Hero(
+                      tag: 'appLogo',
+                      child: Image(
+                        image: AssetImage('images/diteLogo.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Dimensions.d_30,
+                  ),
+                  UserButton(
+                    text: 'Log Masuk',
+                    height: Dimensions.d_65,
+                    color: Colours.grey,
+                    onClick: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                  ),
+                  UserButton(
+                    text: 'Daftar Sebagai Pengguna',
+                    height: Dimensions.d_65,
+                    color: Colours.lightBlue,
+                    onClick: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                  ),
+                  UserButton(
+                    text: 'Daftar Sebagai JBIM',
+                    height: Dimensions.d_65,
+                    color: Colours.blue,
+                    onClick: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage(isSLI: true)),
+                      );
+                    },
+                  ),
+                ],
               ),
-              SizedBox(
-                height: Dimensions.d_30,
-              ),
-              UserButton(
-                text: 'Log Masuk Sini!',
-                height: Dimensions.d_65,
-                color: Colours.grey,
-                onClick: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-              ),
-              UserButton(
-                text: 'Daftar Sini!',
-                height: Dimensions.d_65,
-                color: Colours.blue,
-                onClick: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
