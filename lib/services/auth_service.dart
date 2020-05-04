753d48a3397707b59423f5de8ec57cb96b3e143b
Hun Zi-Song <zhun0001@student.monash.edu>
2020-05-04 17:34:12 +0800

    Added pop function to remove verification page before pushing navigation page

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:heard/home/navigation.dart';
 import 'package:heard/startup/startup_page.dart';
 
@@ -23,6 +22,7 @@ class AuthService {
       print("Error caught signing in");
     }).then((user) {
       if (user != null) {
+        Navigator.pop(context);
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => Navigation()),
