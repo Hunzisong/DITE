6d9bdd873b0992e23374c805900aea15844a8f3c
Hun Zi-Song <zhun0001@student.monash.edu>
2020-05-17 09:06:35 +0800

    Changed directory name to suit naming standard

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:heard/Videochatcomponents/index.dart';  // the video call join page
+import 'package:heard/video_chat_components/index.dart';  // the video call join page
 
 class Home extends StatefulWidget {
   @override
