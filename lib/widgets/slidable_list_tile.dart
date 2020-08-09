6fb8041c0c6ff36922f45c93e2a2e919cef31a8e
Hun Zi-Song <zhun0001@student.monash.edu>
2020-08-10 03:19:02 +0800

    Refactored slidable list tile to accept more arguments for more features

--- lib/widgets/slidable_list_tile.dart
+++ lib/widgets/slidable_list_tile.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:heard/constants.dart';
+import 'package:heard/home/on_demand/on_demand_sli_page.dart';
 
 class SlidableListTile extends StatelessWidget {
-  final Function onTap;
-  final String name;
+  final Function onAccept;
+  final UserInfoTemp userInfo;
+  final bool isThreeLine;
+  final Widget onTrailingButtonPress;
 
-  SlidableListTile({this.onTap, this.name});
+  SlidableListTile({this.onAccept, this.userInfo, this.isThreeLine = false, this.onTrailingButtonPress});
 
   @override
   Widget build(BuildContext context) {
@@ -17,18 +21,27 @@ class SlidableListTile extends StatelessWidget {
           actionExtentRatio: 0.25,
           child: ListTile(
             contentPadding: EdgeInsets.all(Dimensions.d_20),
+            isThreeLine: isThreeLine,
             leading: Icon(
               Icons.account_circle,
-              size: Dimensions.d_45,
+              size: Dimensions.d_55,
             ),
-            title: Text('Nama: $name'),
+            title: Text('${userInfo.userName}', style: TextStyle(fontWeight: FontWeight.bold),),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('${userInfo.hospital}', style: TextStyle(color: Colours.darkGrey),),
+                userInfo.isEmergency ? Text('*Kecemasan', style: TextStyle(color: Colours.fail),) : SizedBox.shrink(),
+              ],
+            ),
+            trailing: onTrailingButtonPress != null ? onTrailingButtonPress : SizedBox.shrink(),
           ),
           secondaryActions: <Widget>[
             IconSlideAction(
                 caption: 'Terima',
                 color: Colours.accept,
                 icon: Icons.done,
-                onTap: onTap),
+                onTap: onAccept),
           ],
         ),
         Divider(
