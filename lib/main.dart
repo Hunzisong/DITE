27bb435d2612147eecf0ed87663f3dd6e295296e
Hun Zi-Song <zhun0001@student.monash.edu>
2020-08-26 17:59:23 +0800

    Added new update to firebase (firebase.initializeApp)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:heard/landing/landing_page.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 //import 'package:heard/schedule/schedule_page.dart'; //for testing purpose
 
-
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
 
 class App extends StatelessWidget {
 
