e3e52992641649f1023c394bff19229a660d6cba
Hun Zi-Song <zhun0001@student.monash.edu>
2020-08-27 22:43:45 +0800

    Added new grey title bar widget

---
+++ lib/widgets/grey_title_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:heard/constants.dart';
+
+class GreyTitleBar extends StatelessWidget {
+  final String title;
+  final Widget trailing;
+
+  GreyTitleBar({this.title, this.trailing});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colours.grey,
+      padding: EdgeInsets.symmetric(
+          horizontal: Dimensions.d_20, vertical: Dimensions.d_10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing
+        ],
+      ),
+    );
+  }
+}
