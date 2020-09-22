6be9971f1356a49c9b256c1937aaa841339e8016
Bryan Yong <byon0001@student.monash.edu>
2020-09-23 00:17:53 +0800

    Added covid-19 questionnaire UI screen

---
+++ lib/home/covid19/questionnaire.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class Questionnaire extends StatefulWidget {
+  @override
+  _QuestionnaireState createState() => _QuestionnaireState();
+}
+
+class _QuestionnaireState extends State<Questionnaire> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('Covid-19'),
+
+      ),
+      body: SafeArea(
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+}
