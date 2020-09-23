a55c63a2c5718b317ada9c97456f0a166e684a52
Hun Zi-Song <zhun0001@student.monash.edu>
2020-09-23 22:52:02 +0800

    Added information page for sli and user in transaction tab

--- lib/http_services/booking_services.dart
+++ lib/http_services/booking_services.dart
@@ -64,97 +64,39 @@ class BookingServices {
     return allTransactions;
   }
 
-//  Future<bool> acceptOnDemandRequest({String headerToken, String onDemandID}) async {
-//    var response = await http
-//        .post('https://heard-project.herokuapp.com/ondemand/accept', headers: {
-//      'Authorization': headerToken,
-//    }, body: {
-//      'on_demand_id': onDemandID,
-//    });
-//
-//    print('Accept On-Demand Request: ${response.statusCode}, body: ${response.body}');
-//
-//    if (response.statusCode == 200) {
-//      return true;
-//    }
-//    else {
-//      return false;
-//    }
-//  }
-//
-//  Future<OnDemandStatus> getOnDemandStatus({String headerToken, @required bool isSLI}) async {
-//    var response = await http.get('https://heard-project.herokuapp.com/ondemand/status?type=${isSLI ? 'sli' : 'user'}',
-//      headers: {
-//        'Authorization': headerToken,
-//      },
-//    );
-//
-//    print('Get On-Demand Status: ${response.statusCode}, body: ${response.body}');
-//
-//    OnDemandStatus onDemandStatus;
-//    if (response.statusCode == 200) {
-//      Map<String, dynamic> statusBody = jsonDecode(response.body);
-//      onDemandStatus = OnDemandStatus.fromJson(statusBody);
-//    }
-//
-//    return onDemandStatus;
-//  }
-//
-//  Future<String> makeOnDemandRequest({String headerToken, OnDemandInputs onDemandInputs}) async {
-//    var response = await http.post('https://heard-project.herokuapp.com/ondemand/request',
-//        headers: {
-//          'Authorization': headerToken,
-//        },
-//        body: {
-//          'hospital': onDemandInputs.hospital.text,
-//          'hospital_department': onDemandInputs.department.text,
-//          'emergency': onDemandInputs.isEmergency.toString(),
-//          'on_behalf': onDemandInputs.isBookingForOthers.toString(),
-//          'gender': onDemandInputs.genderType.toString().split('.').last,
-//          'patient_name': onDemandInputs.patientName.text,
-//          'note': onDemandInputs.noteToSLI.text,
-//        }
-//    );
-//
-//    if (response.statusCode == 200) {
-//      print("On-Demand Request Success");
-//      return "Successful request";
-//    }
-//    else {
-//      print("On-Demand Request Failed");
-//      return "Failed request";
-//    }
-//  }
-//
-//  Future<String> cancelOnDemandRequest({String headerToken}) async {
-//    var response = await http.post('https://heard-project.herokuapp.com/ondemand/cancel',
-//        headers: {
-//          'Authorization': headerToken,
-//        }
-//    );
-//
-//    if (response.statusCode == 200) {
-//      print("On-Demand Cancel Success");
-//      return 'Successfully Cancelled Request';
-//    }
-//    else {
-//      print("On-Demand Cancel Failed");
-//      return 'Failed to Cancel Request';
-//    }
-//  }
-//
-//  Future<String> endOnDemandRequest({String headerToken}) async {
-//    var response = await http.post('https://heard-project.herokuapp.com/ondemand/end',
-//        headers: {
-//          'Authorization': headerToken,
-//        }
-//    );
-//
-//    if (response.statusCode == 200) {
-//      return 'Successfully Ended Request';
-//    }
-//    else {
-//      return 'Failed to End Request';
-//    }
-//  }
+  Future<bool> cancelBooking({String headerToken, String bookingID}) async {
+    var response = await http
+        .post('https://heard-project.herokuapp.com/booking/cancel', headers: {
+      'Authorization': headerToken,
+    }, body: {
+      'booking_id': bookingID,
+    });
+
+    print('Cancel Booking Response: ${response.statusCode}, body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+ Future<bool> finishBooking({String headerToken, String bookingID}) async {
+   var response = await http
+       .post('https://heard-project.herokuapp.com/booking/end', headers: {
+     'Authorization': headerToken,
+   }, body: {
+     'booking_id': bookingID,
+   });
+
+   print('End/Finish Booking Response: ${response.statusCode}, body: ${response.body}');
+
+   if (response.statusCode == 200) {
+     return true;
+   }
+   else {
+     return false;
+   }
+ }
 }
