f066cff2c983e7327cb650c7cb3d9844adaa6756
Bryan Yong <byon0001@student.monash.edu>
2020-09-23 12:50:22 +0800

    Added simple search bar and icon into the covid19 UI screen

--- lib/home/covid19/questionnaire.dart
+++ lib/home/covid19/questionnaire.dart
@@ -1,7 +1,10 @@
 
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:heard/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
+
 
 
 
@@ -38,11 +41,43 @@ class _QuestionnaireState extends State<Questionnaire> {
         title: Text('Covid-19'),
 
       ),
-      body: SafeArea(
-        child: Column(
+      body: Column(
+
+          children: <Widget>[
+
+            Container(
+              height: 100.0,
+              child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colours.grey,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                                          Radius.circular(40.0),
+                        ),
+                    ),
+                    labelText: 'Search...',
+                    icon: Icon(Icons.search, color: Colours.black,)
+                  ),
+                ),
+              padding: EdgeInsets.all(30),
+            ),
+
+
+            // search bar for the UI
+
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text('Adakah anda mengalami gejala-gejala ini dalam tempoh 14 hari yang lepas?') ,
+            ),
+
+
+
+
+          ],
+
+
 
         ),
-      ),
     );
   }
 }
