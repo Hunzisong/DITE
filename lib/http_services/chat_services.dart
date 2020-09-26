49524ae488a376dd2caef7b9ea45779339345446
Bryan Yong <byon0001@student.monash.edu>
2020-09-26 14:46:24 +0800

    Added chat item package in the chat http service file

--- lib/http_services/chat_services.dart
+++ lib/http_services/chat_services.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:heard/api/chat_item.dart';
 
 
 class ChatServices {
 
+
+  // create 3 different methods to get 3 different APIs
 
 
 
