5e784ceb43e17fd9c8356ac17c273219c3e10f54
Bryan Yong <byon0001@student.monash.edu>
2020-09-26 20:59:03 +0800

    Completed in writing a method to retrieve chat room lists of each user from backend API

--- lib/http_services/chat_services.dart
+++ lib/http_services/chat_services.dart
@@ -9,6 +9,98 @@ class ChatServices {
 
   // create 3 different methods to get 3 different APIs
 
+  /* First method is to retrieve the chat room list
+     for each authenticated users.
+     This API is used to populate chat history
+   */
+
+  Future<List<ChatItem>> getChatRoomList(
+      {String headerToken, bool isSLI}) async {
+    var response = await http.get(
+        'https://heard-project.herokuapp.com/chat/all?type=${isSLI ? 'sli' : 'user'}',
+        headers: {
+          'Authorization': headerToken,
+        });
+
+    print('Get all chat list: ${response.statusCode}, body: ${response.body}');
+
+    /**
+     * Create an empty list to retrieve the data obtained from API
+     */
+    List<ChatItem> allChatRooms = [];
+
+    if (response.statusCode == 200) {
+      List<dynamic> requestsBody = jsonDecode(response.body);
+      for (int i = 0; i < requestsBody.length; i++) {
+        ChatItem request = ChatItem.fromJson(requestsBody[i]);
+        allChatRooms.add(request);
+      }
+    }
+
+    return allChatRooms;
+  }
+
+
+
+  /* Second method for each authenticated users.
+
+     This API should be called when user/SLI enter chat room in an ongoing session
+     (because front-end won't know if they have chat history or not),
+     it will return necessary data for front-end to populate and listen to fire store.
+
+   */
+  Future<bool> enterChatRoom(
+      {String headerToken, String bookingID, bool isAcceptBooking}) async {
+    var response = await http.post(
+        'https://heard-project.herokuapp.com/chat/enter',
+        headers: {
+          'Authorization': headerToken,
+        },
+        body: {
+          'booking_id': bookingID,
+          'response': isAcceptBooking ? 'accept' : 'decline'
+        });
+
+    print(
+        'Posted SLI Response: ${response.statusCode}, body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  /* Third method for each authenticated users.
+
+     This API is used to send chat message to counterpart,
+     it will store the message in firestore and trigger notification
+     on couterpart's device.
+
+   */
+  Future<bool> sendChatMessage(
+      {String headerToken, String bookingID, bool isAcceptBooking}) async {
+    var response = await http.post(
+        'https://heard-project.herokuapp.com/chat/send',
+        headers: {
+          'Authorization': headerToken,
+        },
+        body: {
+          'booking_id': bookingID,
+          'response': isAcceptBooking ? 'accept' : 'decline'
+        });
+
+    print(
+        'Posted SLI Response: ${response.statusCode}, body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
 
 }
