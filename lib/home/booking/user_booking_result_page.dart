86b75ec91f20dd7bba2ce4a1f6731b0b4261fa96
XinXianLim <xlim0001@student.monash.edu>
2020-10-11 16:51:17 +0800

    Fix text overflow issue in card - user booking result page

--- lib/home/booking/user_booking_result_page.dart
+++ lib/home/booking/user_booking_result_page.dart
@@ -209,13 +209,16 @@ class _UserBookingResultPageState extends State<UserBookingResultPage> {
             ),
             Padding(
               padding: EdgeInsets.all(Dimensions.d_20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  RichTextField("Nama", name),
-                  RichTextField("Jantina", gender),
-                  //RichTextField("Umur", age),
-                ],
+              child: Container(
+                width: Dimensions.d_200+30,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    RichTextField("Nama", name),
+                    RichTextField("Jantina", gender),
+                    //RichTextField("Umur", age),
+                  ],
+                ),
               ),
             ),
           ],
